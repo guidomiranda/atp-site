f9aa0989fe6881755a41e4f17efc4512e79f181f
fervillalbag <21fervillalbag@gmail.com>
2022-07-10 23:38:36 -0400

    feat: clients page fetch info from database

--- packages/web/src/pages/clientes.tsx
+++ packages/web/src/pages/clientes.tsx
@@ -1,42 +1,54 @@
 import React from 'react';
 import { Box, Heading, Text } from '@chakra-ui/react';
+import { GetServerSideProps } from 'next';
 
 import Header from '../components/client/Header';
 import Layout from '../layout';
+import { getClients } from '../utils';
 
-const ItemReview: React.FC = () => {
+interface ClientsProps {
+	clients: any;
+}
+
+interface ClientProps {
+	client: any;
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const clients = await getClients();
+	return { props: { clients } };
+};
+
+const ItemReview: React.FC<ClientProps> = ({ client }) => {
 	return (
-		<Box as='article' bgColor='#155e9d' p='32px'>
-			<Heading
-				fontSize={['32px', '42px']}
-				fontWeight='normal'
-				color='white'
-				// textTransform='uppercase'
-			>
-				Cliente 1 - Fábrica Cavallaro
+		<Box as='article' bgColor='#155e9d' p='32px' mb='56px'>
+			<Heading fontSize={['32px', '42px']} fontWeight='normal' color='white'>
+				{client.title}
 			</Heading>
 			<Box mt='20px'>
-				<Text color='white' fontSize={['16px', '20px']} mt='15px'>
-					Puesto que creemos que la atención a nuestros clientes es tan
-					importante como el crecimiento profesional y personal de nuestros
-					compañeros
-				</Text>
-				<Text color='white' fontSize={['16px', '20px']} mt='15px'>
-					¡Contactá con nosotros para más info o realizar tus pedidos, nos
-					aseguramos de entregar tu producto en cualquier punto!
-				</Text>
+				{client.description.map(item => (
+					<Text color='white' fontSize={['16px', '20px']} mt='15px' key={item}>
+						{item}
+					</Text>
+				))}
 			</Box>
+			<Text color='white' fontSize={['16px', '20px']} mt='15px'>
+				¡Contactá con nosotros para más info o realizar tus pedidos, nos
+				aseguramos de entregar tu producto en cualquier punto!
+			</Text>
 		</Box>
 	);
 };
 
-const Clientes = () => {
+const Clientes: React.FC<ClientsProps> = ({ clients }) => {
 	return (
 		<Layout>
 			<Header title='Conoce a nuestros clientes' image='/img_clientes.png' />
-			<Box bgColor='#0a101e' py='94px'>
+			<Box bgColor='#0a101e' pt='94px' pb='38'>
 				<Box maxW='1220px' w='90%' m='0 auto'>
-					<ItemReview />
+					{clients.clients?.map(item => (
+						<ItemReview key={item.id} client={item} />
+					))}
 				</Box>
 			</Box>
 		</Layout>
