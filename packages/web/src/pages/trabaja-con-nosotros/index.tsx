ca1502c054ce34d3be74c7b973d4a966a6bc7c41
fervillalbag <21fervillalbag@gmail.com>
2022-07-25 12:19:22 -0400

    feat: page trabaja-con-nosotros

--- packages/web/src/pages/trabaja-con-nosotros/index.tsx
+++ packages/web/src/pages/trabaja-con-nosotros/index.tsx
@@ -13,55 +13,64 @@ const TrabajaConNosotros = () => {
 				bgImage="url('/fondo-2.jpg')"
 				bgSize='cover'
 				bgPos='center'
-				h='500px'
+				h='400px'
 			>
 				<Box maxW='1220px' w='90%' m='0 auto' h='full'>
-					<Flex position='relative' display='inline-flex'>
-						<Heading fontSize='48px' fontWeight='black' color='#fff'>
-							Trabaja con nosotros
-						</Heading>
-						<Box
-							position='absolute'
-							width='25px'
-							height='12px'
-							bgColor='#b41f1b'
-							right='0'
-							bottom='-12px'
-						/>
-					</Flex>
+					<Flex
+						justifyContent='center'
+						h='full'
+						// alignItems='center'
+						flexDir='column'
+					>
+						<Box ml={['0', '72px']}>
+							<Flex position='relative' display='inline-flex'>
+								<Heading fontSize='48px' fontWeight='black' color='#fff'>
+									Trabaja con nosotros
+								</Heading>
+								<Box
+									position='absolute'
+									width='25px'
+									height='12px'
+									bgColor='#b41f1b'
+									right='0'
+									bottom='-12px'
+								/>
+							</Flex>
+						</Box>
 
-					<Flex alignItems='center' justifyContent='center' h='full'>
-						<Box mx='32px'>
-							<Button
-								textTransform='uppercase'
-								color='white'
-								bgColor='#b41f1b'
-								rounded='0'
-								h='45px'
-								p='0 32px'
-								textAlign='center'
-								_hover={{ bgColor: '#b41f1b' }}
-								_active={{ bgColor: '#b41f1b' }}
-							>
-								Búsquedas Vigentes
-							</Button>
-						</Box>
-						<Box mx='32px'>
-							<Button
-								textTransform='uppercase'
-								color='white'
-								bgColor='#b41f1b'
-								rounded='0'
-								h='45px'
-								p='0 32px'
-								textAlign='center'
-								_hover={{ bgColor: '#b41f1b' }}
-								_active={{ bgColor: '#b41f1b' }}
-								onClick={() => router.push('/trabaja-con-nosotros/cv')}
-							>
-								DÉJANOS TU CV
-							</Button>
-						</Box>
+						<Flex mt='70px' flexDir={['column', 'row']} justifyContent='center'>
+							<Box mx={['0', '32px']}>
+								<Button
+									textTransform='uppercase'
+									color='white'
+									bgColor='#b41f1b'
+									rounded='0'
+									h='45px'
+									p='0 32px'
+									textAlign='center'
+									_hover={{ bgColor: '#b41f1b' }}
+									_active={{ bgColor: '#b41f1b' }}
+								>
+									Búsquedas Vigentes
+								</Button>
+							</Box>
+							<Box mx={['0', '32px']} mt={['20px', '0']}>
+								<Button
+									textTransform='uppercase'
+									color='white'
+									bgColor='#b41f1b'
+									rounded='0'
+									h='45px'
+									p='0 32px'
+									textAlign='center'
+									_hover={{ bgColor: '#b41f1b' }}
+									_active={{ bgColor: '#b41f1b' }}
+									onClick={() => router.push('/trabaja-con-nosotros/cv')}
+								>
+									DÉJANOS TU CV
+								</Button>
+							</Box>
+						</Flex>
 					</Flex>
 				</Box>
 			</Box>
