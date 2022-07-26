df24923c722ed8aad2ce1e323a5d849b25232814
fervillalbag <21fervillalbag@gmail.com>
2022-07-26 12:09:14 -0400

    fix: info products

--- packages/web/src/data/product.ts
+++ packages/web/src/data/product.ts
@@ -3,7 +3,7 @@ export const products = [
 		id: '1',
 		title: 'Lubricantes',
 		description: [
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ullam aliquid, fuga quia a quis voluptates quam tempore deserunt nostrum vero sunt neque eius maiores voluptatum sequi consectetur beatae quaerat molestiae at iste. Optio reprehenderit id vitae a animi quis.',
+			'Mobil tiene una línea de lubricantes exclusivas para cada segmento de operación que cumple con las especificaciones mas diversas del mercado, ya sea con Mobil Delvac TM para camiones pesados y livianos, Mobil Super TM para automóviles, Mobil Super Moto TM para motos, y Mobil TM para industrias, generamos movimiento para todo tipo de vehículos y motores.',
 		],
 		more_info: false,
 		image: 'https://atp.netlify.app/assets/img-lubricantes-mobil.png',
@@ -14,7 +14,7 @@ export const products = [
 		id: '2',
 		title: 'Baterías',
 		description: [
-			'Con la moderna tecnología de electrolitos absorbida en las mantas de fibra de vidrio (AGM), las baterías Route no requie- ren mantenimiento, lo que brinda mayor practicidad, seguri- dad, durabilidad y tranquilidad al usuario. Con certificación de calidad ISO 9001 desde 2010 e ISO 14001 desde el 2011, que certi- fica la responsabilidad de preservar el medio ambiente; bate- rías Route tiene un laboratorio de pruebas técnicas, moderno y bien equipado donde profesionales altamente calificados eva- lúan la calidad de los productos de acuerdo con los estándares brasileños (ABNT) e internacionales (JIS).',
+			'Con la moderna tecnología de electrolitos absorbida en las mantas de fibra de vidrio (AGM), las baterías Route no requieren mantenimiento, lo que brinda mayor practicidad, seguridad, durabilidad y tranquilidad al usuario. Con certificación de calidad ISO 9001 desde 2010 e ISO 14001 desde el 2011, que certifica la responsabilidad de preservar el medio ambiente; baterías Route tiene un laboratorio de pruebas técnicas, moderno y bien equipado donde profesionales altamente calificados evalúan la calidad de los productos de acuerdo con los estándares brasileños (ABNT) e internacionales (JIS).',
 		],
 		more_info: true,
 		image: 'https://atp.netlify.app/assets/img-baterias-route.png',
@@ -25,8 +25,7 @@ export const products = [
 		id: '3',
 		title: 'Filtros',
 		description: [
-			'Vox, es una marca moderna, audaz y creativa de origen brasilero perteneciente a un importante holding de empresas y marcas relacionadas a las autopartes.',
-			'Vox desarrolla y suministra filtros automotrices e industriales con altos estándares de calidad para líneas livianas, pesadas, agrícolas y especiales. ATP comercializa actualmente en Paraguay, filtros para línea liviana.',
+			'Vox, es una marca moderna, audaz y creativa de origen brasilero perteneciente a un importante holding de empresas y marcas relacionadas a las autopartes. Vox desarrolla y suministra filtros automotrices e industriales con altos estándares de calidad para líneas livianas, pesadas, agrícolas y especiales. ATP comercializa actualmente en Paraguay, filtros para línea liviana.',
 			'Vox es reconocida en el mercado por su flexibilidad y capacidad de innovación, utilizando alta tecnología para lanzar nuevos productos y estar al tanto de la evolución del mundo de los filtros.',
 			'La infraestructura de su fábrica, con modernos equipos, profesionales calificados y en constante capacitación sumado al servicio ágil y eficaz garantizan la total satisfacción de sus clientes.',
 		],
