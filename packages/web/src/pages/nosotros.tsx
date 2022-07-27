4e5508256613a647dec8ee8eda72f058f398c357
fervillalbag <21fervillalbag@gmail.com>
2022-07-27 14:54:50 -0400

    fix: color at nosotros page

--- packages/web/src/pages/nosotros.tsx
+++ packages/web/src/pages/nosotros.tsx
@@ -14,15 +14,15 @@ const ValuesQuote: React.FC<ValuesQuoteProps> = ({ title, description }) => {
 	return (
 		<Box position='relative' as='article' mb='20px'>
 			<Box ml='36px'>
-				<Text fontWeight='bold'>
+				<Text fontWeight='bold' color='#fff'>
 					{title}{' '}
-					<Text fontWeight='normal' display='inline'>
+					<Text fontWeight='normal' display='inline' color='#fff'>
 						{description}
 					</Text>
 				</Text>
 			</Box>
 			<Box position='absolute' top='2px' left='0'>
-				<Text color='#b41f1b' fontSize='24px'>
+				<Text color='#fff' fontSize='24px'>
 					<FaAngleDoubleRight />
 				</Text>
 			</Box>
@@ -38,157 +38,166 @@ const About = () => {
 				title='Quienes somos'
 				description='ATP es una empresa con más de 40 años en el mercado que se encarga de distribuir productos y servicios de alta calidad.'
 			/>
-			<Box maxW='1220px' m='0 auto' w='90%' py='128px'>
-				<Box as='article' mb='96px'>
-					<Grid
-						gridTemplateColumns={{ base: '1fr', md: 'repeat(2,1fr)' }}
-						gap={{ base: '48px 0', md: '0 64px' }}
-					>
-						<Box>
-							<Flex display='inline-flex' position='relative'>
-								<Heading
-									fontWeight='black'
-									color='#015796'
-									fontSize={{ base: '40px', md: '56px' }}
+			<Box bgImage='linear-gradient(rgb(13, 20, 38), rgb(87, 86, 84))'>
+				<Box maxW='1220px' m='0 auto' w='90%' py='128px'>
+					<Box as='article' mb='96px'>
+						<Grid
+							gridTemplateColumns={{ base: '1fr', md: 'repeat(2,1fr)' }}
+							gap={{ base: '48px 0', md: '0 64px' }}
+						>
+							<Box>
+								<Flex display='inline-flex' position='relative'>
+									<Heading
+										fontWeight='black'
+										color='#fff'
+										fontSize={{ base: '40px', md: '56px' }}
+									>
+										Misión
+									</Heading>
+									<Box
+										width='25px'
+										height='12px'
+										bgColor='#fff'
+										position='absolute'
+										right='0'
+										bottom='-12px'
+									/>
+								</Flex>
+								<Text
+									fontSize={['18px', '20px']}
+									mt='26px'
+									w={['100%', '90%']}
+									color='#fff'
 								>
-									Misión
-								</Heading>
-								<Box
-									width='25px'
-									height='12px'
-									bgColor='#b41f1b'
-									position='absolute'
-									right='0'
-									bottom='-12px'
+									Brindar soluciones mediante la distribución eficiente de
+									productos y servicios de alta calidad respaldados por un
+									equipo humano capacitado y comprometido.
+								</Text>
+							</Box>
+							<Box>
+								<Image
+									src='/img-local-atp.jpg'
+									alt=''
+									width='100%'
+									objectFit='cover'
+									verticalAlign='top'
 								/>
-							</Flex>
-							<Text fontSize={['18px', '20px']} mt='26px' w={['100%', '90%']}>
-								Brindar soluciones mediante la distribución eficiente de
-								productos y servicios de alta calidad respaldados por un equipo
-								humano capacitado y comprometido.
-							</Text>
-						</Box>
-						<Box>
-							<Image
-								src='/img-local-atp.jpg'
-								alt=''
-								width='100%'
-								objectFit='cover'
-								verticalAlign='top'
-							/>
-						</Box>
-					</Grid>
+							</Box>
+						</Grid>
+					</Box>
+					<Box as='article' mb='32px'>
+						<Grid
+							gridTemplateColumns={{ base: '1fr', md: 'repeat(2,1fr)' }}
+							gap={{ base: '48px 0', md: '0 64px' }}
+						>
+							<Box>
+								<Flex display='inline-flex' position='relative'>
+									<Heading
+										fontWeight='black'
+										color='#fff'
+										fontSize={{ base: '40px', md: '56px' }}
+									>
+										Visión
+									</Heading>
+									<Box
+										width='25px'
+										height='12px'
+										bgColor='#fff'
+										position='absolute'
+										right='0'
+										bottom='-12px'
+									/>
+								</Flex>
+								<Text
+									fontSize={['18px', '20px']}
+									mt='26px'
+									w={['100%', '90%']}
+									color='#fff'
+								>
+									Proveer la mejor experiencia personalizada con un equipo
+									humano de primer nivel.
+								</Text>
+							</Box>
+							<Box>
+								<Image
+									src='/img-equipo-atp.jpg'
+									alt=''
+									width='100%'
+									objectFit='cover'
+									verticalAlign='top'
+								/>
+							</Box>
+						</Grid>
+					</Box>
 				</Box>
-				<Box as='article' mb='32px'>
-					<Grid
-						gridTemplateColumns={{ base: '1fr', md: 'repeat(2,1fr)' }}
-						gap={{ base: '48px 0', md: '0 64px' }}
-					>
-						<Box>
-							<Flex display='inline-flex' position='relative'>
-								<Heading
-									fontWeight='black'
-									color='#015796'
-									fontSize={{ base: '40px', md: '56px' }}
-								>
-									Visión
-								</Heading>
-								<Box
-									width='25px'
-									height='12px'
-									bgColor='#b41f1b'
-									position='absolute'
-									right='0'
-									bottom='-12px'
-								/>
-							</Flex>
-							<Text fontSize={['18px', '20px']} mt='26px' w={['100%', '90%']}>
-								Proveer la mejor experiencia personalizada con un equipo humano
-								de primer nivel.
-							</Text>
-						</Box>
-						<Box>
-							<Image
-								src='/img-equipo-atp.jpg'
-								alt=''
-								width='100%'
-								objectFit='cover'
-								verticalAlign='top'
-							/>
-						</Box>
-					</Grid>
+
+				<Box py='72px'>
+					<Box maxW='1220px' m='0 auto' w='90%'>
+						<Grid
+							gridTemplateColumns={{ base: '1fr', md: 'repeat(2,1fr)' }}
+							gap={{ base: '48px 0', md: '0 72px' }}
+						>
+							<Box>
+								<Flex display='inline-flex' position='relative'>
+									<Heading
+										fontWeight='black'
+										color='#fff'
+										fontSize={{ base: '40px', md: '56px' }}
+									>
+										Valores
+									</Heading>
+									<Box
+										width='25px'
+										height='12px'
+										bgColor='#fff'
+										position='absolute'
+										right='0'
+										bottom='-12px'
+									/>
+								</Flex>
+
+								<Box mt='38px'>
+									<ValuesQuote
+										title='Integridad.'
+										description='Actuamos con transparencia y honestidad en cada una de nuestras gestiones.'
+									/>
+									<ValuesQuote
+										title='Trabajo en Equipo.'
+										description=' Generamos sinergia con acciones alineadas a un objetivo común.'
+									/>
+									<ValuesQuote
+										title='Innovación.'
+										description='Comprometidos con mejoras continua en todos los niveles.'
+									/>
+									<ValuesQuote
+										title='Seguridad.'
+										description='Con los clientes internos, externos y el medio ambiente.'
+									/>
+									<ValuesQuote
+										title='Desarrollo humano.'
+										description='Estabilidad laboral y capacitación.'
+									/>
+									<ValuesQuote
+										title='Valor compartido.'
+										description='Equilibrio en el desarrollo económico, social y ambiental.'
+									/>
+								</Box>
+							</Box>
+							<Box>
+								<Image src='/img-valores-atp.jpg' alt='' />
+							</Box>
+						</Grid>
+					</Box>
 				</Box>
 			</Box>
 
 			<Box bgColor='#f8f8f9' py='72px'>
-				<Box maxW='1220px' m='0 auto' w='90%'>
-					<Grid
-						gridTemplateColumns={{ base: '1fr', md: 'repeat(2,1fr)' }}
-						gap={{ base: '48px 0', md: '0 72px' }}
-					>
-						<Box>
-							<Flex display='inline-flex' position='relative'>
-								<Heading
-									fontWeight='black'
-									color='#015796'
-									fontSize={{ base: '40px', md: '56px' }}
-								>
-									Valores
-								</Heading>
-								<Box
-									width='25px'
-									height='12px'
-									bgColor='#b41f1b'
-									position='absolute'
-									right='0'
-									bottom='-12px'
-								/>
-							</Flex>
-
-							<Box mt='38px'>
-								<ValuesQuote
-									title='Integridad.'
-									description='Actuamos con transparencia y honestidad en cada una de nuestras gestiones.'
-								/>
-								<ValuesQuote
-									title='Trabajo en Equipo.'
-									description=' Generamos sinergia con acciones alineadas a un objetivo común.'
-								/>
-								<ValuesQuote
-									title='Innovación.'
-									description='Comprometidos con mejoras continua en todos los niveles.'
-								/>
-								<ValuesQuote
-									title='Seguridad.'
-									description='Con los clientes internos, externos y el medio ambiente.'
-								/>
-								<ValuesQuote
-									title='Desarrollo humano.'
-									description='Estabilidad laboral y capacitación.'
-								/>
-								<ValuesQuote
-									title='Valor compartido.'
-									description='Equilibrio en el desarrollo económico, social y ambiental.'
-								/>
-							</Box>
-						</Box>
-						<Box>
-							<Image src='/img-valores-atp.jpg' alt='' />
-						</Box>
-					</Grid>
-				</Box>
-			</Box>
-
-			<Box
-				bgImage='linear-gradient(rgb(13, 20, 38), rgb(87, 86, 84))'
-				py='72px'
-			>
 				<Grid placeItems='center' maxW='1000px' w='90%' m='0 auto'>
 					<Box textAlign='center' mb='56px'>
 						<Flex display='inline-flex' position='relative'>
 							<Heading
 								fontWeight='black'
-								color='#fff'
+								color='#015796'
 								fontSize={{ base: '32px', md: '42px' }}
 							>
 								Vídeo Institucional
